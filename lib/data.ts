f161a0fbaf72e33f35de30d6a3a085237871c5ee
Vitaly Kharlakov <vkharlakov@yandex.ru>
2023-09-15 18:36:47 +0200

    Add skills const

--- lib/data.ts
+++ lib/data.ts
@@ -22,3 +22,78 @@ export const links = [
     hash: "#contacts",
   },
 ] as const;
+
+export const skills = [
+  {
+    name: "TypeScript",
+    size: "wide",
+  },
+  {
+    name: "Node.js",
+    size: "wide",
+  },
+  {
+    name: "HTML",
+    size: "small",
+  },
+  {
+    name: "CSS",
+    size: "small",
+  },
+  {
+    name: "Express.js",
+    size: "small",
+  },
+  {
+    name: "MongoDB",
+    size: "small",
+  },
+  {
+    name: "mongoose",
+    size: "small",
+  },
+  {
+    name: "React",
+    size: "tall",
+  },
+  {
+    name: "Redux",
+    size: "small",
+  },
+  {
+    name: "Adaptive",
+    size: "medium",
+  },
+  {
+    name: "JavaScript",
+    size: "small",
+  },
+  {
+    name: "Next.js",
+    size: "small",
+  },
+  {
+    name: "Nginx",
+    size: "small",
+  },
+  {
+    name: "Docker",
+    size: "small",
+  },
+  {
+    name: "BEM",
+    size: "small",
+  },
+  {
+    name: "Figma",
+    size: "small",
+  },
+  {
+    name: "GitHub",
+    size: "small",
+  },
+  {
+    name: "Frame Motion",
+    size: "small",
+  },
+] as const;
