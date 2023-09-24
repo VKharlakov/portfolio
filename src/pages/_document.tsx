5791bb04856efee757f168caeb02bdcc24a0cfce
Vitaly Kharlakov <vkharlakov@yandex.ru>
2023-09-25 00:56:06 +0200

    Temporary changed language to ru

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,9 +2,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="ru">
       <Head />
-      <body className="body-document">
+      <body>
         <Main />
         <NextScript />
       </body>
